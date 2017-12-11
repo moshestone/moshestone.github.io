02e09a16842020076bd8ced810829506c7bc638d
moshestone <34243791+moshestone@users.noreply.github.com>
2017-12-11 09:26:36 +0000

    Add files via upload

---
+++ camp canada/js/slide.js
@@ -0,0 +1,61 @@
+// faq popup
+$(document).ready(function(){
+  $("#q1").click(function(){
+    $("#a1").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#q2").click(function(){
+    $("#a2").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#q3").click(function(){
+    $("#a3").slideToggle("slow");
+  });
+});
+
+// $(document).ready(function(){
+//   $("#q4").click(function(){
+//     $("#a4").slideToggle("slow");
+//   });
+// });
+//
+// $(document).ready(function(){
+//   $("#q5").click(function(){
+//     $("#a5").slideToggle("slow");
+//   });
+// });
+
+$(document).ready(function(){
+  $("#q6").click(function(){
+    $("#a6").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#q7").click(function(){
+    $("#a7").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#q8").click(function(){
+    $("#a8").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#q9").click(function(){
+    $("#a9").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#q10").click(function(){
+    $("#a10").slideToggle("slow");
+  });
+});
+// end of faq popup
